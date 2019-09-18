176f890eb3910484d755987b4afd3374ab51d376
mr.judo <enovoa_man@hotmail.com>
2019-09-19 04:18:43 +0800

    second

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require("config")
+const db = config.get("mongoURI")
+
+
+const connetDB = async ()=>{
+    try{
+       await mongoose.connect(db)
+       console.log("mongodb connected...")
+    } catch(err){
+        console.log(err.message)
+        // exit process with failure
+        process.exit(1)
+    }
+}
+module.exports = connetDB
